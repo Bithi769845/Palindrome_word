bdbf7a84a1e59da98c757115940ca3e7df47ae17
Ms.Bithi <bithimony01904@gmail.com>
2024-10-02 15:16:19 +0600

    Problem solved

---
+++ palindrome words/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace palindrome_words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a string");
+            string str = Console.ReadLine();
+            char[] StringArr = str.ToCharArray();
+            Array.Reverse(StringArr);
+            string ReverseString = new string(StringArr);
+            Console.WriteLine(ReverseString);
+
+            Console.ReadLine();
+            if (str == ReverseString)
+            {
+                Console.WriteLine(str + " is palindrome word");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(str + " is not palindrome word");
+                Console.ReadLine();
+            }
+        }
+    }
+}
